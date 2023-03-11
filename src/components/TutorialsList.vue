91cd9c39fe3f0d3ce76bd0917660b57fa3e22457
Romi Julianto <romyjulians@gmail.com>
2023-03-11 22:39:17 +0700

    add: main component TutorialsList

---
+++ src/components/TutorialsList.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="list row">
+        <div class="col-md-8">
+            <div class="input-group mb-3">
+                <input 
+                type="text" 
+                class="form-control" 
+                placeholder="Search by Title"
+                v-model="title"
+                />
+
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button">
+                        Search
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <h4>Tutorials List</h4>
+            <ul class="list-group">
+                <li class="list-group-item">
+                    
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import TutorialDataServices from '@/services/TutorialDataServices';
+
+export default {
+    name: "tutorials-list",
+    data() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style>
+.list {
+    text-align: left;
+    max-width: 750px;
+    margin: auto;
+}
+</style>
