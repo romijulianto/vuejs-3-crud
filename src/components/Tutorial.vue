0ba755f3cf703c7949909b49f8d7237831645512
Romi Julianto <romyjulians@gmail.com>
2023-03-11 23:59:58 +0700

    add: main component Tutorials

---
+++ src/components/Tutorial.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import TutorialDataServices from '@/services/TutorialDataServices';
+
+export default {
+  name: "tutorial-data",
+  data() {
+    return {
+      cureentTutorial: null,
+      message: ''
+    };
+  },
+  methods: {
+
+  },
+
+  mounted() {
+    this.message = '';
+  }
+}
+</script>
+
+<style>
+.edit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
