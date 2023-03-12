dbcc417a1cc0c1254000c285e2ad0dd663927354
Romi Julianto <romyjulians@gmail.com>
2023-03-12 23:34:17 +0700

    add: finish client/forntend

---
+++ client/README.md
@@ -0,0 +1,29 @@
+# Build CRUD Application with VueJS 3
+
+## Setup and Depedencies
+```
+- vue create vue-3-crud
+- cd vue-3-crud
+- npm install bootstrap@4.6.0 jquery popper.js
+- npm install vue-router@4
+- npm install axios
+- 
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ client/src/components/Tutorial.vue
@@ -0,0 +1,130 @@
+<template>
+  <div v-if="currentTutorial" class="edit-form">
+  <h4>Tutorial</h4>
+  <form>
+    <div class="form-group">
+      <label for="title">Title</label>
+      <input type="text" class="form-control" id="title"
+      v-model="currentTutorial.title"
+      />
+    </div>
+    <div class="form-group">
+      <label for="description">Description</label>
+      <input type="text" class="form-control" id="description"
+      v-model="currentTutorial.description"
+      />
+    </div>
+
+    <div class="form-group">
+      <label><strong>Status</strong></label>
+      {{ currentTutorial.published ? "Published" : "Pending" }}
+    </div>
+  </form>
+
+  <button class="badge badge-primary mr-2"
+  v-if="currentTutorial.published"
+  @click="updatePublished(false)">
+    UnPublish
+  </button>
+  <button v-else class="badge badge-primary mr-2"
+  @click="updatePublished(true)">
+    Publish
+  </button>
+
+  <button class="badge badge-danger mr-2"
+  @click="deleteTutorial">
+    Delete
+  </button>
+
+  <button type="submit" class="badge badge-success"
+  @click="updateTutorial">
+    Update
+  </button>
+  <p>{{  message }}</p>
+  </div>
+
+  <div v-else>
+    <br/>
+    <p>Please click on a Tutorial...</p>
+  </div>
+</template>
+
+<script>
+import TutorialDataServices from '@/services/TutorialDataServices';
+
+export default {
+  name: "tutorial-data",
+  data() {
+    return {
+      currentTutorial: null,
+      message: ''
+    };
+  },
+  methods: {
+    getTutorial(id) {
+      TutorialDataServices.get(id)
+      .theb(response => {
+        this.currentTutorial = response.data;
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      })
+    },
+
+    updatePublished(status) {
+      var data = {
+        id: this.currentTutorial.id,
+        title: this.currentTutorial.title,
+        description: this.currentTutorial.description,
+        published: status
+      }
+
+      TutorialDataServices.update(this.currentTutorial.id, data)
+      .then(response => {
+        console.log(response.data);
+        this.currentTutorial.published = status;
+        this.message = 'The status was uupdated succesfully!';
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    },
+
+    updateTutorial() {
+      TutorialDataServices.update(this.currentTutorial.id, this.currentTutorial)
+      .then(response => {
+        console.log(response.data);
+        this.message = 'The tutorial was updated succesfully!';
+      })
+      .catch(e => {
+        console.log(e);
+      })
+    },
+
+    deleteTutorial() {
+      TutorialDataServices.delete(this.currentTutorial.id)
+      .then(response => {
+        console.log(response.data);
+        this.$router.push({ name: "tutorials" })
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    }
+
+  },
+
+  mounted() {
+    this.message = '';
+    this.getTutorial(this.$route.params.id);
+  }
+}
+</script>
+
+<style>
+.edit-form {
+  max-width: 300px;
+  margin: auto;
+}
+</style>
